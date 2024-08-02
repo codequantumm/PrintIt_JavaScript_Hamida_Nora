72b774b94b8367fb3e03c8469aa89909e5d15156
Nora Hamida <hamida.nora@gmail.com>
2024-08-02 14:18:02 +0200

    Supression bullets dans html pour intégration dans JS

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,4 @@
-
-
-const flecheGauche = document.querySelector(".arrow_left")
-flecheGauche.addEventListener("click", () => {
-	console.log("Tu as cliqué sur la flèche de Gauche ! Bravo")
-})
-
-const flecheDroite = document.querySelector(".arrow_right")
-flecheDroite.addEventListener("click", () => {
-	console.log("Tu as cliqué sur la flèche de Droite !")
-})
-
-
-const slides = [
+slides  = [
 	{
 		"image":"slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
@@ -29,3 +16,34 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+flecheGauche = document.querySelector(".arrow_left")
+flecheGauche.addEventListener("click", () => {
+	console.log("Tu as cliqué sur la flèche de Gauche ! Bravo")
+})
+flecheDroite = document.querySelector(".arrow_right")
+flecheDroite.addEventListener("click", () => {
+	console.log("Tu as cliqué sur la flèche de Droite !")
+})
+
+
+bulletPointContainer = document.getElementById("dots")
+bulletPointContainer.innerHTML =`
+
+				<div class="dot"></div>
+				<div class="dot"></div>
+				<div class="dot"></div>
+				<div class="dot"></div>
+
+	`
+	console.log(bulletPointContainer) 
+
+
+	// Ajout des bullets points au html
+
+/*let slideClickButton = document.querySelector(".dots")
+for (let i=0; i< slides.length; i++) {
+
+} */
+
+
