504c3be634efcb5d74882920574102c555d43266
Nora Hamida <hamida.nora@gmail.com>
2024-08-07 14:55:43 +0200

    Déclaration des variables du DOM

--- assets/script.js
+++ assets/script.js
@@ -17,10 +17,6 @@ slides  = [
 	}
 ]
 
-
-
-
-
 // Sélection des éléments du DOM
 const bannerImg = document.querySelector('.banner-img');
 const bannerText = document.querySelector('#banner p');
@@ -31,41 +27,3 @@ const arrowRight = document.querySelector('.arrow_right');
 let indexActuel= 0;
 
 
-// Initialisation des points (bullet points) 8 plusieurs points s'affichaient car espace dans le html)
-function initDots() {
-	slides.forEach((slide, index) => {
-		const dot = document.createElement('div');
-		dot.classList.add('dot');
-		if (index === 0) dot.classList.add('dot_selected');
-		dotsContainer.appendChild(dot);
-	});
-}
-
-// Mise à jour de l'affichage
-function updateSlide(index) {
-    bannerImg.src = `./assets/images/slideshow/${slides[index].image}`;
-    bannerText.innerHTML = slides[index].tagLine;
-
-    document.querySelector('.dot.dot_selected').classList.remove('dot_selected');
-    dotsContainer.children[index].classList.add('dot_selected');
-}
-
-// Gestionnaire de clic pour la flèche droite
-arrowRight.addEventListener('click', () => {
-    indexActuel = (indexActuel + 1) % slides.length;
-    updateSlide(indexActuel);
-
-	console.log("Tu as cliqué sur la flèche de Droite !")
-});
-
-// Gestionnaire de clic pour la flèche gauche
-arrowLeft.addEventListener('click', () => {
-    indexActuel = (indexActuel - 1 + slides.length) % slides.length;
-    updateSlide(indexActuel);
-
-	console.log("Tu as cliqué sur la flèche de Gauche !")
-});
-
-// Initialisation
-initDots();
-updateSlide(indexActuel);
